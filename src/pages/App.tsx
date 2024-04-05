ac406d41a0ab6929425cacb2e83abaca50181897
toastmanager <mrbonkyou@gmail.com>
2024-04-06 01:34:03 +0900

    first commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+import MyMap from '../components/MyMap'
+import '../styles/App.css'
+
+function App() {
+  return (
+    <>
+      <div className='w-[1000px]'>
+        <MyMap />
+      </div>
+    </>
+  )
+}
+
+export default App
