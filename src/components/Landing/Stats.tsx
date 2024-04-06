16ba4a4f73e03b3562a0bc837f8cdeab7649bead
toastmanager <mrbonkyou@gmail.com>
2024-04-06 23:23:02 +0900

    landing stats

--- src/components/Landing/Stats.tsx
+++ src/components/Landing/Stats.tsx
@@ -7,7 +7,7 @@ const LandingStats = () => {
                 <h1 className="font-bold text-[36px] text-center">Немного статистики о туризме</h1>
                 <p className="text-center">Cъешь ещё этих мягких французских булок, да выпей же чаю</p>
             </div>
-            <div>
+            <div className='grid grid-cols-2 gap-[28px]'>
                 <div className="w-[595px] h-[595px] rounded-[15px] bg-winter-cian relative flex justify-center">
                     <div className='absolute bottom-[75px] z-10 text-white'>
                         <h1 className='text-[36px] font-bold'>Людей заинтерисовано</h1>
@@ -16,15 +16,26 @@ const LandingStats = () => {
                     <div className="bg-gradient-to-t from-winter-cian via-transparent from-10 opacity-70 absolute w-full h-full rounded-[15px] z-0"></div>
                     <img src={image2} className='w-full h-full object-cover rounded-[15px]'/>
                 </div>
-                <div></div>
+                <div className='grid gap-[32px] grid-cols-2 grid-rows-2'>
+                    <StatsBlock text='Точность прогноза' value={52} unit='%'/>
+                    <StatsBlock text='Статистика' value={32} unit='М'/>
+                    <StatsBlock text='Статистика' value={60} unit='М'/>
+                    <StatsBlock text='Статистика' value={125} unit='+'/>
+                </div>
             </div>
         </div>
     )
 }
 
-const StatsBlock = () => {
+const StatsBlock = (props: {text: string, value: number, unit: string}) => {
     return (
-        <></>
+        <div className='border border-winter-light-purple h-[284px] w-[284px] rounded-[15px] py-[53px] px-[30px] text-winter-cian flex flex-col justify-end'>
+            <h2 className='text-[20px] font-bold'>{props.text}</h2>
+            <h1 className='text-[44px] font-bold'>
+                {props.value}
+                <span className='text-winter-blue'>{props.unit}</span>
+            </h1>
+        </div>
     )
 }
 export default LandingStats
